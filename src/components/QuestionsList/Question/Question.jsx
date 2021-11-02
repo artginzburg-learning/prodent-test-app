e83c24ef7902b97ce595b758e40dcd3458370e4b
artginzburg <art.ginzburg@ya.ru>
2021-11-02 22:04:24 +0300

    Reload image caps with holder.js after page change

--- src/components/QuestionsList/Question/Question.jsx
+++ src/components/QuestionsList/Question/Question.jsx
@@ -1,9 +1,19 @@
+import Holder from 'holderjs';
+import { useEffect, useRef } from 'react';
 import { Card, Button } from 'react-bootstrap';
 
 export default function Question({ question }) {
+  const imageRef = useRef();
+
+  useEffect(() => {
+    Holder.run({
+      images: imageRef.current,
+    });
+  }, [imageRef]);
+
   return (
     <Card style={{ width: '255px' }}>
-      <Card.Img variant="top" src="holder.js/100px143?theme=card" />
+      <Card.Img ref={imageRef} variant="top" src="holder.js/100px143?theme=card" />
       <Card.Body>
         <Card.Title>{question.title}</Card.Title>
         <Card.Text>{question.description}</Card.Text>
